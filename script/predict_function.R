b9658ec53fc5751b8a5126ef0ba8534aad9c8ce8
karleg <32402869+karleg@users.noreply.github.com>
2021-08-30 02:43:00 -0400

    weighted version of enrichment

--- script/predict_function.R
+++ script/predict_function.R
@@ -1,68 +1,77 @@
-# This script is runafter fast_function.R and uses data structures created there, therefore it is loading
-# an .RData file.   The prediction proceeds as follows:  For each isoform, look at the distribution 
-# of GO terms of other isoforms in its community (communities are assigned by the infomap algorithm in
-# the script faster_function.R). Go terms that appear more than expected by chance(with respect to their
-# frequency in the population, i.e all the genes in the data), and also belong to the gene that contains the
-# isoform, are selected as the isoform's functions.
-
+#use after fast_function.R,  this scripts uses data structures from the previous script
 library(parallel)
+
+library(igraph)
+
+library(goseq)
 
 load('function.RData')
 
+gene.functions=getgo(unique(interpro.tab$ensembl_gene_id),'hg38','ensGene',fetch.cats="GO:BP")
+
 n.cores=32
 
-function.counts=table(unlist(gene.functions)) #frequency of GO terms in the population
+function.counts=table(unlist(gene.functions))
 
-#Assign function to each isoform:
+num.genes.with.functions=sum(unlist(lapply(gene.functions,function(l)length(unlist(l))>0)))
 
 isoform.functions=do.call(rbind,mclapply(isoforms,function(isoform)
 {
-  
-  #Get the functions/GO terms of the gene that contains the isoform
-  
   cur.funcs=gene.functions[genes[which(isoforms==isoform)]]
   
   gene=genes[isoforms==isoform]
  
-  #If the gene does not have functions associated with it the isoform does not either:
-  
   if (length(unlist(cur.funcs))==0)
     
-    return(matrix(nrow=0,ncol=3))
-  
-  #Count the number of times each GO term appears in other community members:
+    return(matrix(nrow=0,ncol=4))
    
-  cur.counts=unlist(lapply(unlist(cur.funcs),function(x)sum(unlist(gene.functions[genes[(res$membership==res$membership[which(isoforms==isoform)]) & isoforms!=isoform]]) %in% unlist(x))))
+  adj.vtx=ends(g, E(g)[from(which(isoforms==isoform))])[,1]
+  
+  if (length(adj.vtx)==0)
+    
+    return(matrix(nrow=0,ncol=4))
+  
+  if (adj.vtx[1]==which(isoforms==isoform))
+    
+    adj.vtx=ends(g, E(g)[from(which(isoforms==isoform))])[,2]
+    
+  e.weights=get.edge.attribute(g,'weight',E(g)[from(which(isoforms==isoform))])
+    
+  e.weights=e.weights[which(adj.vtx %in% which((res$membership==res$membership[which(isoforms==isoform)]) & isoforms!=isoform))]
+    
+  adj.vtx=adj.vtx[which(adj.vtx %in% which((res$membership==res$membership[which(isoforms==isoform)]) & isoforms!=isoform))]
+    
+  if (length(unlist(adj.vtx))==0)
+    
+    return(matrix(nrow=0,ncol=4))
+  
+  cur.counts=unlist(lapply(unlist(cur.funcs),function(x)sum(unlist(lapply(1:length(adj.vtx),function(i)sum(e.weights[i]*gene.functions[[genes[adj.vtx[i]]]] %in% unlist(x)))))))
     
   names(cur.counts)=unlist(cur.funcs)
   
-  cur.counts=cur.counts[cur.counts>0] #keep terms that appear at least once
+  cur.counts=cur.counts[cur.counts>0]
   
   if (length(unlist(cur.funcs))==0)
     
-    return(matrix(nrow=0,ncol=3))
-  
-  #Use the binomial test to get a p-value for each term to assess its over-representation:
+    return(matrix(nrow=0,ncol=4))
   
   p.vals=c()
   
   for (func in names(cur.counts))
   {
     
-    p.head=function.counts[func]/length(unique(genes))
+    p.head=function.counts[func]/num.genes.with.functions
     
-    p.vals=c(p.vals,pbinom(q=cur.counts[func]-1,size=sum((res$membership==res$membership[which(isoforms==isoform)]) & isoforms!=isoform),prob=p.head,lower.tail=FALSE))
+    p.vals=c(p.vals,pbinom(q=round(cur.counts[func])-1,size=round(sum(e.weights)),prob=p.head,lower.tail=FALSE))
     
   }
   
-  #return the p-values found for this isoforms and the GO terms
-  
-  matrix(c(rep(isoform,length(cur.counts)),names(cur.counts),p.vals),ncol=3)
+  matrix(c(rep(isoform,length(cur.counts)),names(cur.counts),p.vals,p.adjust(p.vals,method='BH')),ncol=4)
   
 },mc.cores=n.cores))
 
-isoform.functions=cbind(isoform.functions,p.adjust(isoform.functions[,3]))
-
 colnames(isoform.functions)=c('Ensembl ID','Go Terms','P.Val','P.adjusted')
+
+#isoform.functions=isoform.functions[isoform.functions[,4]<=0.05,]
 
 write.table(isoform.functions,'predicted_isoform_functions.txt',sep='\t',quote=F,row.names=F,col.names=T)
