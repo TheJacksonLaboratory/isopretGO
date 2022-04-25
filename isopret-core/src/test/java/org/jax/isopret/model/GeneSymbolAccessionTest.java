eb5691c965bf5dc4e4d6097cb9d1b809e0b0d081
Peter Robinson <peter.robinson@jax.org>
2022-04-25 07:17:26 -0400

    unit tests for GeneSymbolAccession

---
+++ isopret-core/src/test/java/org/jax/isopret/model/GeneSymbolAccessionTest.java
@@ -0,0 +1,21 @@
+package org.jax.isopret.model;
+
+import org.jax.isopret.core.model.AccessionNumber;
+import org.jax.isopret.core.model.GeneSymbolAccession;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GeneSymbolAccessionTest {
+
+
+    @Test
+    public void testConstructorAccession() {
+        AccessionNumber brca2 = AccessionNumber.ensemblGene("ENSG00000139618.2");
+        String geneSymbol = "BRCA2";
+        GeneSymbolAccession gsa = new GeneSymbolAccession(geneSymbol, brca2);
+        String expectedLabel = "BRCA2 (ENSG00000139618)";
+        assertEquals(expectedLabel, gsa.toString());
+    }
+
+}
