bffac67bebd84ebec25b986e24a49abff1c24505
Peter Robinson <peter.robinson@jax.org>
2022-04-25 07:00:50 -0400

    removing erroneous default PEP threshold for visualizing significant isoforms

--- isopret-core/src/main/java/org/jax/isopret/core/hbadeals/HbaDealsTranscriptResult.java
+++ isopret-core/src/main/java/org/jax/isopret/core/hbadeals/HbaDealsTranscriptResult.java
@@ -7,8 +7,6 @@ public class HbaDealsTranscriptResult {
     private final double foldChange;
     private final double P;
 
-    private final double DEFAULT_THRESHOLD = 0.13;
-
 
     public HbaDealsTranscriptResult(AccessionNumber transcript, double fc, double p) {
         this.transcript = transcript;
@@ -36,10 +34,6 @@ public class HbaDealsTranscriptResult {
 
     public double getP() {
         return P;
-    }
-
-    public boolean isSignificant() {
-        return isSignificant(DEFAULT_THRESHOLD);
     }
 
     public boolean isSignificant(double threshold) {
