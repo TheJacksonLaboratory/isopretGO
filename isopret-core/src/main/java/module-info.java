3ed9cd7dbe5ec2c1aa69bab724467554889b6603
Peter Robinson <peter.robinson@jax.org>
2022-08-04 15:33:16 -0400

    adding module-info

---
+++ isopret-core/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.jax.isopret.core {
+    requires org.slf4j;
+    requires org.monarchinitiative.phenol.core;
+    requires org.monarchitiative.svart;
+    requires org.monarchinitiative.phenol.analysis;
+    requires guava;
+    requires jannovar.core;
+    requires org.apache.commons.net;
+
+}
