d47f192680168f73be26deda64b1a30822552736
Peter Robinson <peter.robinson@jax.org>
2023-07-06 08:35:15 -0400

    updating pom

--- isopret-core/src/main/java/module-info.java
+++ isopret-core/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.jax.isopret.core {
     requires org.slf4j;
     requires org.monarchinitiative.phenol.core;
-    requires org.monarchitiative.svart;
+    requires org.monarchinitiative.svart;
     requires org.monarchinitiative.phenol.analysis;
     requires jannovar.core;
     requires org.apache.commons.net;
