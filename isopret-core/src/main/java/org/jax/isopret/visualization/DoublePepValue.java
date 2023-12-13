bfe063f19d482e38854deef89f5375a71448e8b1
Peter Robinson <peter.robinson@jax.org>
2023-12-13 10:59:15 +0100

    adding comparable interface

--- isopret-core/src/main/java/org/jax/isopret/visualization/DoublePepValue.java
+++ isopret-core/src/main/java/org/jax/isopret/visualization/DoublePepValue.java
@@ -5,7 +5,13 @@ package org.jax.isopret.visualization;
  * and whether they were determined to be significant given the current
  * dataset and FDR threshold.
  */
-public record DoublePepValue(double pep, boolean isSignificant) {
+public record DoublePepValue(double pep, boolean isSignificant) implements Comparable<DoublePepValue> {
 
 
+    @Override
+    public int compareTo(DoublePepValue o) {
+        if (this.pep < o.pep) return -1;
+        else if (this.pep > o.pep) return 1;
+        else return 0;
+    }
 }
